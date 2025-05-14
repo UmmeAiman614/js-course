59f47370c79d1a632f34204e6c8a58b9ed767d1a
UmmeAiman614 <165355872+UmmeAiman614@users.noreply.github.com>
2025-05-14 13:49:00 +0500

    js course variables

---
+++ basicVar.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail= "ummeaiman@gmail.com"
+var accountPassword = "345678"
+accountCity = "Jhelum"
+let accountState;
+// accountId = 2 not allowewd
+console.log(accountId);
+accountEmail = "ua@gmail.com"
+accountPassword="212121"
+accountCity = "chiniot"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
