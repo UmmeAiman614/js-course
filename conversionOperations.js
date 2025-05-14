533bd283bae89e3999a2055f79076b7b906c65a7
UmmeAiman614 <165355872+UmmeAiman614@users.noreply.github.com>
2025-05-14 14:42:54 +0500

    conversion of datatypes

---
+++ conversionOperations.js
@@ -0,0 +1,32 @@
+// let score = "33abs";
+// let score = null;
+// let score = undefined;
+let score = true;
+
+
+
+
+
+// console.log(typeof(score));
+
+let valueInNumber= Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+let isLoggedIn = "umme";
+
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "Umme" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
