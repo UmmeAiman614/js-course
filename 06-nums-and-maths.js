abd3eefb53d206d1c4b038b16a88e3633f75d7bc
UmmeAiman614 <165355872+UmmeAiman614@users.noreply.github.com>
2025-07-01 13:12:37 +0500

    nums and maths

---
+++ 06-nums-and-maths.js
@@ -0,0 +1,37 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 21123.567
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+
+// ****************************MATHS*******************************
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //it just changes negative value to positive
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2)); //it takes only top value like it takes only 5
+// console.log(Math.floor(4.9));  //it takes only lower value like it takes only 4
+// console.log(Math.min(4,3,6,7));
+// console.log(Math.max(4,3,6,7));
+
+console.log(Math.random());
+const random = console.log(Math.floor(Math.random()*5) + 1);
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor((Math.random() * (max-min + 1)) + min));
+
--- strings.js
+++ strings.js
@@ -4,13 +4,27 @@ const repocount = 43;
 // console.log(name + repocount + "Value")
 
 console.log(`Hello my name is ${name} and my repocounts are ${repocount}`);
-const gameName = new String("Umme Aiman");
+const gameName = new String("Umme-Aiman-zafar");
 
-console.log(gameName[5]);
-console.log(gameName.__proto__)
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf('a'));
+// console.log(gameName[5]);
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 5)
+console.log(anotherString);
+
+const newStringOne = "     Umme     "
+console.log(newStringOne);
+console.log(newStringOne.trim()); //trim removes extra spaces
 
 
+const url = 'https://ummeAiman%10zafar'
+console.log(url.replace('%10', '%30')); //replace replaces value which we want
+console.log(url.includes('junaid'));
+console.log(gameName.split('a'));
